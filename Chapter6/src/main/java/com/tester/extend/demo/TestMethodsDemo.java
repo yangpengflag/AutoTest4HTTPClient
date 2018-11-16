1fbc6f8d13af69c7eb92dc8ebb902e12879a1926
yangpengflag <38930516+yangpengflag@users.noreply.github.com>
2018-11-16 18:53:44 +0800

    new submit

---
+++ Chapter6/src/main/java/com/tester/extend/demo/TestMethodsDemo.java
@@ -0,0 +1,28 @@
+package com.tester.extend.demo;
+
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TestMethodsDemo {
+
+    @Test
+    public void test1(){
+        Assert.assertEquals(1,2);
+    }
+
+    @Test
+    public void test2(){
+        Assert.assertEquals(1,1);
+    }
+    @Test
+    public void test3(){
+        Assert.assertEquals("aaa","aaa");
+    }
+
+    @Test
+    public void testDemo(){
+        Reporter.log("这是我们自己写的日志");
+        throw new RuntimeException("这是我们自己写的运行时一次");
+    }
+}
